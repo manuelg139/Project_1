eb77f759259b8d25f240f33756b678938cc6288c
andrbj <brandonjandreae@gmail.com>
2021-02-03 21:15:16 -0600

    omdbapi js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var saveBtn = document.querySelector("#saveBtn");
 var userInput = document.querySelector("#search-hero");
 var marvelApiKey = "?apikey=8544f15bee458e41b62dad89c68ebf90";
-var omdbApiKey = "";
+var omdbApiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=4e80b14d";
 var selects = JSON.parse(localStorage.getItem('selects')) || [];
