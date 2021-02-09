43b67c2980919b8321d1c38100a90249fe327fe6
andrbj <brandonjandreae@gmail.com>
2021-02-09 10:41:48 -0600

    omdb API working

--- assets/omdbApi.js
+++ assets/omdbApi.js
@@ -1,24 +1,41 @@
 $(document).ready(() => {
-    $('#searchForm').on('submit' , (e) => {
-        let searchText = $('#searchText').val();
-        getMovies(searchText);
-        e.preventDefault();
-    })
-});
+    // $('#searchForm').on('keyup' , (e) => {
+    //     console.log("Checked")
+    //     console.log(e)
+    //     if(e.code === "Enter"){
+    //     e.preventDefault();
+    //     let searchText = $('#searchText').val();
+    //     console.log(searchText)
+    //     getMovies(searchText);
+    // }
 
+    // })
+
+$(".submitBtn").on("click", function(e){
+e.preventDefault();
+console.log("Clicked")
+        let searchText = $('#searchText').val();
+        console.log(searchText)
+        getMovies(searchText);
+})
 function getMovies(searchText){
-    axios.get('http://www.omdbapi.com/?apikey=8544f15bee458e41b62dad89c68ebf90&language=en-US&query=' + searchText)
+    $.ajax({
+        type: "GET",
+        url:'http://www.omdbapi.com/?apikey=4e80b14d&t=' + searchText
+    })
     .then((response) => {
         console.log(response);
-        let movies = response.data.Search;
-        let output = '';
-        $.each(movies, (index, movie) =>{
-            output += `
+        // let movies = response.data.Search;
+        // let output = '';
+        // $.each(movies, (index, movie) =>{
+        //     output += `
            
-            `;
-        });  
+        //     `;
+        // });  
     })
     .catch((err) => {
         console.log(err);
-    });
-} 
+    })
+}
+});
+
