bad5c4402f5354c30b4335a797e619dbde2955ce
andrbj <brandonjandreae@gmail.com>
2021-02-08 22:20:39 -0600

    html on line 85 of js

--- assets/script.js
+++ assets/script.js
@@ -82,7 +82,7 @@ $("#saveBtn").on("click", function () {
         // console.log(charSaveArr[i].image)
         newDiv.append(newImg);
 
-        $("#char-line").appendTo("<img src='" + charImg + "'/>");
+        $("#char-line").html("<img src='" + charImg + "'/>");
 
         // newChar = $(".selects").append(character[i]);
         $(".selects").append(character[i]);
