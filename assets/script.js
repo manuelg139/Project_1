cf87e7f24fead7ff73a7ad42e2a27cc79c109f2b
Dora O <dorozco2390@utexas.edu>
2021-02-08 21:45:07 -0600

    updated line 84 to html instead of append

--- assets/script.js
+++ assets/script.js
@@ -81,7 +81,7 @@ $("#saveBtn").on("click", function () {
         // newImg.attr("<img src=" + charSaveArr[i].image + "'>");
         console.log(charSaveArr[i].image)
         newDiv.append(newImg);
-        $("#char-line").append("<img src=" + charImg + "'>");
+        $("#char-line").html("<img src=" + charImg + "'>");
         // newChar = $(".selects").append(character[i]);
         $(".selects").append(character[i]);
         // QUERY = QUERY.slice(0, 6);
