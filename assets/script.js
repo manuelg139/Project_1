5082a37b96cf81875145e2fa172bbcc6b3f06a0d
Dora O <dorozco2390@utexas.edu>
2021-02-09 13:59:29 -0600

    removed previous lines

--- assets/script.js
+++ assets/script.js
@@ -83,10 +83,10 @@ $("#saveBtn").on("click", function () {
         newDiv.append(newImg);
 
         // $("#char-line").html("<img src='" + charSaveArr[i].image + "'/>");
-        newChar = $("<img>").append("<img src='" + charImg[i] + "'/>");
-        // $("#char-line").append("<img src='" + charSaveArr[i].image + "'/>");
-        $("#char-line").append(newChar)
-        newChar = newChar.slice(0, 6);
+        // newChar = $("<img>").append("<img src='" + charImg[i] + "'/>");
+        $("#char-line").append("<img src='" + charSaveArr[i].image + "'/>");
+        // $("#char-line").append(newChar)
+        // newChar = newChar.slice(0, 6);
 
         // newChar = $(".selects").append(character[i]);
         // $(".selects").append(character[i]);
